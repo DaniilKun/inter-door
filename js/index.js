c3bab3b46de1e1127c4511c3c6bd0ce27297e4a0
DaniilKun <96586538+DaniilKun@users.noreply.github.com>
2023-10-26 22:10:54 +0400

    sound click

--- js/index.js
+++ js/index.js
@@ -1,28 +1,36 @@
+const videos = document.querySelectorAll('.video12');
+
 const swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   navigation: {
     nextEl: ".next",
     prevEl: ".prev",
   },
-  loop:true,
+  loop: true,
   mousewheel: true,
   keyboard: true,
+  on: {
+    slideChange: () => handleSlideChange(),
+    transitionEnd: () => handleTransitionEnd(),
+  },
 });
 
 //прием id из localstorege и открытие нужной картинки в слайдере
 let start = localStorage.getItem("SLIDE_ID")
 console.log(start);
-swiper.slideTo( start , 10 ,true )
+swiper.slideTo(start, 10, true);
 
+function handleSlideChange() {
+  pauseVideos();
+}
 
-const videos = document.querySelectorAll('.video12');
-const buttons = document.querySelectorAll('.prev, .next');
+function handleTransitionEnd() {
+  // Ничего не делаем, не вызываем playCurrentVideo()
+}
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    videos.forEach(video => {
-      video.pause();
-      video.currentTime = 0;
-    });
+function pauseVideos() {
+  videos.forEach(video => {
+    video.pause();
+    video.currentTime = 0;
   });
-});
+}
